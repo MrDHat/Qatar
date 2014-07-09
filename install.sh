1087bf6a4d62b8667c04159bc942bae263a1bafc
MrDHat <ktyaks@gmail.com>
2014-07-09 23:59:05 +0530

    Add cors support

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 go get github.com/martini-contrib/render
 go get github.com/go-martini/martini
+go get github.com/martini-contrib/cors
--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
     "github.com/go-martini/martini"
     "github.com/martini-contrib/render"
+    "github.com/martini-contrib/cors"
 )
 
 func main() {
@@ -12,9 +13,15 @@ func main() {
       Charset: "UTF-8", // Sets encoding for json and html content-types. Default is "UTF-8".
       IndentJSON: true, // Output human readable JSON
     }))
+    m.Use(cors.Allow(&cors.Options{
+        AllowOrigins: []string{"*"},
+        AllowMethods: []string{"GET", "POST", "PUT", "PATCH"},
+        ExposeHeaders: []string{"Content-Length"},
+    }))
 
     m.Get("/", func(r render.Render) {
         r.JSON(200, map[string]interface{}{"code": "200","message": "status ok"})
     })
+
     m.Run()
 }
